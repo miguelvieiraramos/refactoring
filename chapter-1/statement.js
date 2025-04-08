60a6ec10569ecc2e515f47a9cdbb0174ed2ea132
Miguel Vieira Ramos <miguelvieiraramos02@gmail.com>
2025-04-08 18:55:33 -0300

    refactor (chapter 1): replace temp with query on the play variable

--- chapter-1/statement.js
+++ chapter-1/statement.js
@@ -1,4 +1,8 @@
 function statement (invoice, plays) {
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID]
+    }
+
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
@@ -7,7 +11,7 @@ function statement (invoice, plays) {
                             minimumFractionDigits: 2 }).format;
 
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
+        const play = playFor(perf)
         let thisAmount = amountFor(perf, play);
 
         // add volume credits
