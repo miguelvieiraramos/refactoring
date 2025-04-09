9a3e122425bf8e60ba73d7d542cfbc6e014736cc
Miguel Vieira Ramos <miguelvieiraramos02@gmail.com>
2025-04-09 19:32:54 -0300

    refactor (chapter 1): extract the creation of the statement to its own function

--- chapter-1/statement.js
+++ chapter-1/statement.js
@@ -19,7 +19,7 @@ function renderPlainText (data) {
     return result;
 }
 
-function statement(invoice, plays) {
+function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
@@ -74,7 +74,11 @@ function statement(invoice, plays) {
     function totalAmount(data) {
         return data.performances.reduce((total, p) => total + p.amount, 0);
     }
-    return renderPlainText(statementData, plays);
+    return statementData
+}
+
+function statement(invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays));
 }
 
 module.exports = statement
