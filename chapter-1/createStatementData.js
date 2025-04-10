3b7701bc71fe711d1ba9053fe7008349a1e7897f
Miguel Vieira Ramos <miguelvieiraramos02@gmail.com>
2025-04-10 07:05:28 -0300

    refactor (chapter 1): move amount calculation to PerformanceCalculator

--- chapter-1/createStatementData.js
+++ chapter-1/createStatementData.js
@@ -6,37 +6,16 @@ function createStatementData(invoice, plays) {
     result.totalAmount = totalAmount(result);
 
     function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(aPerformance);
-        result.amount = amountFor(result)
+        result.play = calculator.play;
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result)
         return result;
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID]
-    }
-
-    function amountFor(aPerformance) {
-        let result = 0;
-        switch (aPerformance.play.type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${aPerformance.play.type}`);
-        }
-        return result;
     }
 
     function volumeCreditsFor(aPerformance) {
@@ -54,6 +33,35 @@ function createStatementData(invoice, plays) {
         return data.performances.reduce((total, p) => total + p.amount, 0);
     }
     return result
+}
+
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+
+    get amount () {
+        let result = 0;
+        switch (this.play.type) {
+        case "tragedy":
+            result = 40000;
+            if (this.performance.audience > 30) {
+                result += 1000 * (this.performance.audience - 30);
+            }
+            break;
+        case "comedy":
+            result = 30000;
+            if (this.performance.audience > 20) {
+                result += 10000 + 500 * (this.performance.audience - 20);
+            }
+            result += 300 * this.performance.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${this.play.type}`);
+        }
+        return result;
+    }
 }
 
 module.exports = createStatementData
