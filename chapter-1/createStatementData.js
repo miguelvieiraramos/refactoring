2dc3da35bb5194d1eb04259758611c0ff35bcab0
Miguel Vieira Ramos <miguelvieiraramos02@gmail.com>
2025-04-10 07:10:34 -0300

    refactor (chapter 1): add createPerformanceCalculator

--- chapter-1/createStatementData.js
+++ chapter-1/createStatementData.js
@@ -6,7 +6,7 @@ function createStatementData(invoice, plays) {
     result.totalAmount = totalAmount(result);
 
     function enrichPerformance(aPerformance) {
-        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
         result.amount = calculator.amount;
@@ -62,6 +62,10 @@ class PerformanceCalculator {
         if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
         return result;
     }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+    return new PerformanceCalculator(aPerformance, aPlay);
 }
 
 module.exports = createStatementData
