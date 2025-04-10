80df6d073097e705240c8ec61f0a74ea41a313f2
Miguel Vieira Ramos <miguelvieiraramos02@gmail.com>
2025-04-10 07:16:59 -0300

    refactor (chapter 1): move amount calculation to the subclasses

--- chapter-1/createStatementData.js
+++ chapter-1/createStatementData.js
@@ -34,26 +34,8 @@ class PerformanceCalculator {
         this.play = aPlay;
     }
 
-    get amount () {
-        let result = 0;
-        switch (this.play.type) {
-        case "tragedy":
-            result = 40000;
-            if (this.performance.audience > 30) {
-                result += 1000 * (this.performance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (this.performance.audience > 20) {
-                result += 10000 + 500 * (this.performance.audience - 20);
-            }
-            result += 300 * this.performance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${this.play.type}`);
-        }
-        return result;
+    get amount() {
+        throw new Error('subclass responsability')
     }
 
     get volumeCreditsFor() {
@@ -73,8 +55,25 @@ function createPerformanceCalculator(aPerformance, aPlay) {
     }
 }
 
-class TragedyCalculator extends PerformanceCalculator {}
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result
+    }
+}
 
-class ComedyCalculator extends PerformanceCalculator {}
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result
+    }
+}
 
 module.exports = createStatementData
