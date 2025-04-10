77106ed84bc57674e740cbcf999325cbc7181ffc
Miguel Vieira Ramos <miguelvieiraramos02@gmail.com>
2025-04-10 06:52:46 -0300

    feat: add htmlStatement

--- chapter-1/statement.js
+++ chapter-1/statement.js
@@ -1,17 +1,6 @@
 const createStatementData = require('./createStatementData');
 
 function renderPlainText (data) {
-    function usd(aNumber) {
-        return new Intl.NumberFormat(
-            "en-US",
-            {
-                style: "currency",
-                currency: "USD",
-                minimumFractionDigits: 2
-            }
-        ).format(aNumber/100);
-    }
-
     let result = `Statement for ${data.customer}\n`;
     for (let perf of data.performances) {   
         result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
@@ -25,6 +14,36 @@ function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
 }
 
-module.exports = statement
+function renderHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+    for (let perf of data.performances) {
+      result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+      result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result;
+}
 
-// parei na 31, preciso fazer a parte do html + mover usd para fora da renderPlainText
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays))
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat(
+        "en-US",
+        {
+            style: "currency",
+            currency: "USD",
+            minimumFractionDigits: 2
+        }
+    ).format(aNumber/100);
+}
+
+module.exports = {
+    statement,
+    htmlStatement,
+}
